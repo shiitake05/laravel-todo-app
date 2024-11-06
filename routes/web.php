9bbd4bca61e50a1a6846c4838d23f7388770d248
shiitake <pegtar377512@gmail.com>
2024-11-06 17:52:02 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('tasks', TaskController::class);
+
+Route::get('/list', [TodoListController::class, 'index']);
